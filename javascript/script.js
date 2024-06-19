b06a049a86584b5aff229c97dadca4e29b1b668b
ThePolarEclipse <58376619+ThePolarEclipse@users.noreply.github.com>
2024-06-19 14:49:28 +0100

    Sticky header

--- javascript/script.js
+++ javascript/script.js
@@ -11,7 +11,37 @@ $('#banner').slick({
 	slidesToScroll: 1,
 	dots: true,
 	autoplay: true,
-	autoplaySpeed: 4000,
+	autoplaySpeed: 5000,
 	arrows: false,
-	cssEase: 'linear',
+});
+
+$(document).ready(function() {
+var lastScrollTop = 0;
+var header = $('header');
+
+	$(window).scroll(function(event) {
+		var st = $(this).scrollTop();
+		var yAxisThreshold = 200;
+
+		if (st > yAxisThreshold) {
+			if (st < lastScrollTop) {
+				// Scrolling up
+				if (!header.hasClass('is-sticky')) {
+					header.sticky({topSpacing: 0, zIndex: 10000});
+					header.removeClass('is-hidden');
+					header.addClass('is-sticky');
+				} else {
+					header.removeClass('is-hidden');
+				}
+			} else {
+				// Scrolling down
+				if (header.hasClass('is-sticky')) {
+					header.addClass('is-hidden');
+				}
+			}
+		}
+
+		lastScrollTop = st;
+	});
+	header.addClass('is-hidden');
 });
