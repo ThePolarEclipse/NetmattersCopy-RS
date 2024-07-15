28426f5a7a7ef61ff9c6150668240f92418f50d9
ThePolarEclipse <58376619+ThePolarEclipse@users.noreply.github.com>
2024-07-15 13:21:30 +0100

    Updated based on feedback

--- javascript/script.js
+++ javascript/script.js
@@ -1,109 +1,121 @@
 $(document).ready(function() {
-	// Check if the cookies are accepted
-	if (getCookie("cookies") === "accepted") {
-		$('#cookie-checker').hide();
-	}
+	$(this).scrollTop(0);
 
-	// Show the cookie checker on button click
-	$('.btn-cookies').on('click', function() {
-		$('#cookie-checker').show();
-	});
-
-	// Hide the cookie checker and set the cookie when accept button is clicked
-	$('.btn-accept').on('click', function() {
-		$('#cookie-checker').hide();
-		setCookie("cookies", "accepted", 365);
-	});
-
-	// Function to get the value of a cookie by name
-	function getCookie(name) {
-		let nameEQ = name + "=";
-		let ca = document.cookie.split(';');
-		for (let i = 0; i < ca.length; i++) {
-			let c = ca[i];
-			while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-		}
-		return "";
-	}
-
-	// Function to set a cookie
-	function setCookie(name, value, days) {
-		let expires = "";
-		if (days) {
-			let date = new Date();
-			date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-			expires = "; expires=" + date.toUTCString();
-		}
-		document.cookie = name + "=" + (value || "") + expires + "; path=/";
-	}
+    handleCookieChecker();
+    initializeCarousels();
+    handleStickyHeader();
+    initializeSideMenu();
 });
 
-//Carousels
-$('.row-partners').slick({
-	slidesToShow: 10,
-	slidesToScroll: 1,
-	autoplay: true,
-	autoplaySpeed: 2000,
-	arrows: false
-});
+// Function to handle cookie checker logic
+function handleCookieChecker() {
+    if (getCookie("cookies") === "accepted") {
+        $('#cookie-wrapper').hide();
+    } else {
+        $('body').addClass('no-scroll');
+    }
 
-$('#banner').slick({
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	dots: true,
-	autoplay: true,
-	autoplaySpeed: 5000,
-	arrows: false,
-});
+    $('.btn-cookies').on('click', function() {
+        $('#cookie-wrapper').show();
+        $('body').addClass('no-scroll');
+    });
 
-//sticky header
+    $('.btn-accept').on('click', function() {
+        $('#cookie-wrapper').hide();
+        $('body').removeClass('no-scroll');
+        setCookie("cookies", "accepted", 365);
+    });
 
-$(document).ready(function() {
-var lastScrollTop = 0;
-var header = $('header');
+    function getCookie(name) {
+        let nameEQ = name + "=";
+        let cookieSplit = document.cookie.split(';');
+        for (let i = 0; i < cookieSplit.length; i++) {
+            let curCookie = cookieSplit[i];
+            while (curCookie.charAt(0) == ' ') curCookie = curCookie.substring(1, curCookie.length);
+            if (curCookie.indexOf(nameEQ) == 0) return curCookie.substring(nameEQ.length, curCookie.length);
+        }
+        return "";
+    }
 
-	$(window).scroll(function(event) {
-		var st = $(this).scrollTop();
-		var yAxisThreshold = 200;
+    function setCookie(name, value, days) {
+        let expires = "";
+        if (days) {
+            let date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    }
+}
 
-		if (st > yAxisThreshold) {
-			if (st < lastScrollTop) {
-				// Scrolling up
-				if (!header.hasClass('is-sticky')) {
-					header.sticky({topSpacing: 0, zIndex: 10000});
-					header.removeClass('is-hidden');
-					header.addClass('is-sticky');
-				} else {
-					header.removeClass('is-hidden');
-				}
-			} else {
-				// Scrolling down
-				if (header.hasClass('is-sticky')) {
-					header.addClass('is-hidden');
-				}
-			}
-		}
+// Function to initialize carousels
+function initializeCarousels() {
+    $('.row-partners').slick({
+        slidesToShow: 10,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false
+    });
 
-		lastScrollTop = st;
-	});
-	header.addClass('is-hidden');
-});
+    $('#banner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+    });
+}
 
-//side menu
-$('#menu').SlideOutPanel({
-	bodyPush: true,
-	breakpoint: "1px",
-    closeBtnSize: '',
-    enableEscapeKey: true,
-    screenOpacity: '1',
-    screenZindex:  '99998',
-    showScreen: false,
-    transition: 'ease',
-    transitionDuration: '0.35s',
-});
+// Function to handle sticky header behavior
+function handleStickyHeader() {
+    var lastScrollTop = 0;
+    var header = $('header');
 
-const slideOutPanel = $('#menu').SlideOutPanel();
-$('.btn-burger').on('click', function() {
-    slideOutPanel.toggle();
-});
+    $(window).scroll(function(event) {
+        var st = $(this).scrollTop();
+        var yAxisThreshold = 200;
+
+        if (st > yAxisThreshold) {
+            if (st < lastScrollTop) {
+                // Scrolling up
+                if (!header.hasClass('is-sticky')) {
+                    header.sticky({topSpacing: 0, zIndex: 10000});
+                    header.removeClass('is-hidden');
+                    header.addClass('is-sticky');
+                } else {
+                    header.removeClass('is-hidden');
+                }
+            } else {
+                // Scrolling down
+                if (header.hasClass('is-sticky')) {
+                    header.addClass('is-hidden');
+                }
+            }
+        }
+
+        lastScrollTop = st;
+    });
+    header.addClass('is-hidden');
+}
+
+// Function to initialize side menu
+function initializeSideMenu() {
+    $('#menu').SlideOutPanel({
+        bodyPush: true,
+        breakpoint: "1px",
+        closeBtnSize: '',
+        enableEscapeKey: true,
+        screenOpacity: '1',
+        screenZindex:  '99998',
+        showScreen: false,
+        transition: 'ease',
+        transitionDuration: '0.35s',
+    });
+
+    const slideOutPanel = $('#menu').SlideOutPanel();
+    $('.btn-burger').on('click', function() {
+        slideOutPanel.toggle();
+    });
+}
