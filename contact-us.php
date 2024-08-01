6f0dc2b1c8206102bbe8766e7a1764dba0fdd657
ThePolarEclipse <58376619+ThePolarEclipse@users.noreply.github.com>
2024-08-01 09:12:07 +0100

    Push

---
+++ contact-us.php
@@ -0,0 +1,239 @@
+<?php
+require 'db_connection.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['user-name'];
+    $email = $_POST['user-email'];
+    $telephone = $_POST['user-tel'];
+    $company = $_POST['company'];
+    $message = $_POST['user-message'];
+    
+    $stmt = $conn->prepare("INSERT INTO messages (Name, Email, Telephone, Company, Message) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssss", $name, $email, $telephone, $company, $message);
+
+    if ($stmt->execute()) {
+        echo '<script type="text/javascript">
+            alert("Message sent!");
+        </script>'; 
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Contact Us | Netmatters</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <link rel="stylesheet" href="javascript/plugins/slick/slick.css">
+
+        <link rel="stylesheet" href="javascript/plugins/jquery-SlideOutPanel-master/dist/css/slide-out-panel.css">
+        
+        <link rel="stylesheet" href="css\application.css">
+    </head>
+    <body>  
+        <button type="button" class="btn-cookies btn">Manage Consent</button>
+        <button type="button" class="btn-chat btn"><span class="icon-chat"></span></button>
+        <div id="cookie-wrapper" style="display:none;">
+            <div id="cookie-checker">
+                <h3>Cookies Policy</h3>
+                <p>
+                    Our website uses cookies. This helps us provide you with a good experience on our website. To see what cookies we use and what they do, and to opt-in on non-essential cookies click "change settings". For a detailed explanation, click on "<a href="#">Privacy Policy</a>" otherwise click "Accept Cookies" to enter.
+                </p>
+                <div class="row">
+                    <a class="btn btn-change">
+                        Change Settings
+                    </a>
+                    <a class="btn btn-accept">
+                        Accept Cookies
+                    </a>
+                </div>
+            </div>
+        </div>
+        <!-- start of the sidebar -->
+        <?php include 'php/menu.php'; ?>
+        <!-- end of the sidebar -->
+        <div id="panel">
+            <?php include 'php/header.php'; ?>
+            <div>
+                <main id="contacts">
+                    <div class="container">
+                        <p class="breadcrumb"><small><strong>Home</strong> / Our Offices</small></p>
+                    </div>
+                    <div class="section-head">
+                        <div class="container">
+                            <h1 class="our-offices">Our Offices</h1>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="office-list">
+                            <div class="office-item cambridge">
+                                <div class="image">
+                                    <a href="#"><img src="images\cambridge.jpg" alt=""></a>
+                                </div>
+                                <div class="content">
+                                    <h2><a href="#">Cambridge Office</a></h2>
+                                    <p>
+                                        Unit 1.31, <br>
+                                        St John's Innovation Centre,<br>
+                                        Cowley Road, Milton,<br>
+                                        Cambridge,<br> CB4 0WS
+                                    </p>
+                                    <div class="tel">
+                                        <a href="" class="text-web">01223 37 57 72</a>
+                                    </div>
+                                    <div class="view-more">
+                                        <a href="#" class="btn btn-web">View More</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="office-item wymondham">
+                                <div class="image">
+                                    <a href="#"><img src="images\wymondham.jpg" alt=""></a>
+                                </div>
+                                <div class="content">
+                                    <h2><a href="#">Wymondham Office</a></h2>
+                                    <p>
+                                        Unit 15,  <br>
+                                        Penfold Drive,<br>
+                                        Gateway 11 Business Park,<br>
+                                        Wymondham, Norfolk, <br>NR18 0WZ
+                                    </p>
+                                    <div class="tel">
+                                        <a href="" class="text-web">01603 70 40 20</a>
+                                    </div>
+                                    <div class="view-more">
+                                        <a href="#" class="btn btn-web">View More</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="office-item yarmouth">
+                                <div class="image">
+                                    <a href="#"><img src="images\yarmouth.jpg" alt=""></a>
+                                </div>
+                                <div class="content">
+                                    <h2><a href="#">Great Yarmouth Office</a></h2>
+                                    <p>
+                                        Suite F23,<br>
+                                        Beacon Innovation Centre,<br>
+                                        Beacon Park, Gorleston,<br>
+                                        Great Yarmouth, Norfolk,<br> 7RA
+                                    </p>
+                                    <div class="tel">
+                                        <a href="" class="text-web">01493 60 32 04</a>
+                                    </div>
+                                    <div class="view-more">
+                                        <a href="#" class="btn btn-web">View More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- EMAIL SECTION -->
+
+                        <div class="contacts">
+                            <div>
+                                <div class="contact-net">
+                                    <p><strong>Email us on:</strong><br></p>
+                                    <p><a href="mailto:sales@netmatters.com" class="h3 text-web">sales@netmatters.com</a></p>
+                                    <p><strong>Business hours:</strong></p>
+                                    <p><strong>Monday - Friday 07:00 - 18:00&nbsp;</strong></p>
+                                </div>
+                                <div class="out-of-hours">
+                                    <div class="question out-of-hours">
+                                            <details id="out-dropdown">
+                                                <summary>
+                                                    <h4>Out of Hours IT Support V</h4>
+                                                </summary>
+                                                <p>Netmatters IT are offering an Out of Hours service for Emergency and Critical tasks.</p>
+                                                <p>
+                                                    <strong>Monday - Friday 18:00 - 22:00</strong>
+                                                    <strong>Saturday 08:00 - 16:00</strong><br>
+                                                    <strong>Sunday 10:00 - 18:00</strong>
+                                                </p>
+                                                <p>To log a critical task, you will need to call our main line number and select Option 2 
+                                                    to leave an Out of Hours&nbsp; voicemail. A technician will contact you on the number 
+                                                    provided within 45 minutes of your call.&nbsp;
+                                                </p>
+                                            </details>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-container">
+                                    <form action="contact-us.php" method="POST" id="contact-form">
+                                        <div class="row">
+                                            <div class="users-form">
+                                                <div class="form-group">
+                                                    <label for="users_name" class="required">Your Name</label>
+                                                    <input class="form-control" type="text" name="user-name" id="users_name" required>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="company_name">Company Name</label>
+                                                    <input class="form-control" type="text" name="company" id="company_name">
+                                                </div>
+                                            </div>
+                                            <div class="users-form">
+                                                <div class="form-group">
+                                                    <label for="email" class="required">Your Email</label>
+                                                    <input class="form-control" type="email" name="user-email" id="emailInput" required>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="telephone" class="required">Your Telephone Number</label>
+                                                    <input class="form-control" type="text" name="user-tel" id="telephone" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="message" class="required">Message</label>
+                                            <textarea class="form-control" name="user-message" cols="50" rows="10" id="message" required>Hi, I am interested in discussing Our Offices solution, could you please give me a call or send an email?</textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="pretty-checkbox" for="input_checkbox">
+                                                <input type="checkbox" class="checkbox_input" id="input_checkbox" name="marketing-pref" required>
+                                                <span class="fake-checkbox"></span>
+                                                <span class="check-text">
+                                                    Please tick this box if you wish to receive marketing information from us. 
+                                                    Please see our
+                                                    <a href="#" target="_blank" rel="noopener noreferrer">Privacy Policy</a>
+                                                    for more information on how we keep your data safe.
+                                                </span> 
+                                            </label>
+                                        </div>
+                                        <div class="captcha">
+                                            <span>
+                                                This site is protected by reCAPTCHA and the Google 
+                                                <a href="#" target="_blank" rel="noopener noreferrer"><u>Privacy Policy</u></a> and 
+                                                <a href="#" target="_blank" rel="noopener noreferrer"><u>Terms of Service</u></a> apply.
+                                            </span>
+                                        </div>
+                                        <div class="action-block">
+                                            <button class="btn btn-primary" type="submit">Send Enquiry</button>
+                                            <small class="helper-text">
+                                                <span class="text-danger">*</span> 
+                                                Fields Required
+                                            </small>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+            <!-- Footer -->
+            <?php include 'php/footer.php'; ?>
+        </div>
+        <script src="javascript/jquery-3.7.1.min.js"></script>
+        <script src="javascript/plugins/slick/slick.min.js"></script>
+        <script src="javascript/plugins/jquery.sticky.js"></script>
+
+        <script src="javascript/plugins/jquery-SlideOutPanel-master/dist/js/slide-out-panel.js"></script> 
+        <script src="javascript/plugins/velocity.min.js"></script> 
+
+        <script src="javascript/script.js"></script>
+    </body>
+</html>
---
+++ php/menu.php
@@ -0,0 +1,178 @@
+<nav id="menu">
+            <section>
+                <div class="sidebar">
+                    <div class="sidebar-small">
+                        <div class="sidebar-bespoke">
+                            <button class="btn btn-side">Contact us</button>
+                            <button class="btn-title btn-title-bespoke"><span class="icon-apps_colour"><br></span><small>bespoke</small><br>software</button>
+                            <ul>
+                                <li>Bespoke CRM</li>
+                                <li>Business Automation</li>
+                                <li>Software Integrations</li>
+                                <li>Mobile App Development</li>
+                                <li>Bespoke Databases</li>
+                                <li>Sharepoint Development</li>
+                                <li>Operational Systems</li>
+                                <li>Business central implementation</li>
+                                <li>Interent if things (IOT) software</li>
+                                <li>Intranet Development</li>
+                                <li>Customer Portal Development</li>
+                                <li>Reporting Hub</li>
+                            </ul>
+                        </div>
+                        <div class="sidebar-it">
+                            <button class="btn-title btn-title-it"><span class="icon-display_colour"><br></span><small>it</small><br>support</button>
+                            <ul>
+                                <li>Managed IT support</li>
+                                <li>Business IT support</li>
+                                <li>Office 365 for business</li>
+                                <li>IT Consultancy</li>
+                                <li>Cloud service provider</li>
+                                <li>Data backup &amp; disaster recovery</li>
+                            </ul>
+                        </div>
+                        <div class="sidebar-digital">
+                            <button class="btn-title btn-title-digital"><span class="icon-bar-graph_colour"><br></span><small>digital</small><br>marketing</button>
+                            <ul>
+                                <li>Search engine optimisation (ESO)</li>
+                                <li>Pay per click advertising (PPC)</li>
+                                <li>Conversion rate optimisation (CRO)</li>
+                                <li>Email Marketing</li>
+                                <li>Social media marketing</li>
+                                <li>Content marketing</li>
+                            </ul>
+                        </div>
+                        <div class="sidebar-telecoms">
+                            <button class="btn-title btn-title-telecoms"><span class="icon-phone-service_colour"><br></span><small>telecoms</small><br>services</button>
+                            <ul>
+                                <li>Business mobile</li>
+                                <li>Hosted voip provider</li>
+                                <li>Business voip systems</li>
+                                <li>Business Broadband</li>
+                                <li>Leased lines provider</li>
+                                <li>3CX systems</li>
+                            </ul>
+                        </div>
+                        <div class="sidebar-web">
+                            <button class="btn-title btn-title-web"><span class="icon-code_colour"><br></span><small>web</small><br>design</button>
+                            <ul>
+                                <li>Bespoke website design</li>
+                                <li>Ecommerce website design</li>
+                                <li>Pay monthly websites</li>
+                                <li>Branding &amp; design</li>
+                                <li>Mobile app development</li>
+                                <li>Web hosting</li>
+                            </ul>
+                        </div>
+                        <div class="sidebar-cyber">
+                            <button class="btn-title btn-title-cyber"><span class="icon-security_colour"><br></span><small>cyber</small><br>security</button>
+                            <ul>
+                                <li>Cyber security Assessment</li>
+                                <li>cyber security management</li>
+                                <li>Cyber Penetration testing</li>
+                                <li>Cyber Essentials Certification</li>
+                                <li>PCI Compliance</li>
+                                <li>Hacking prevention</li>
+                            </ul>
+                        </div>
+                        <div class="sidebar-developer">
+                            <button class="btn-title btn-title-developer"><span class="icon-school-cap_colour"><br></span><small>developer</small><br>course</button>
+                            <ul>
+                                <li>Train for a career in tech</li>
+                                <li>Skills bootcamp</li>
+                                <li>Scion scheme frequently asked questions</li>
+                                <li>Scion Collaborators</li>
+                            </ul>
+                        </div>
+                        <div class="button-list">
+                            <button class="btn btn-side">Our work</button>
+                            <button class="btn btn-side">Case studies</button>
+                            <button class="btn btn-side">Our IT clients</button>
+                            <button class="btn btn-side">Our knowledge</button>
+                            <button class="btn btn-side">Technologies</button>
+                            <button class="btn btn-side">News</button>
+                            <button class="btn btn-side">Insights</button>
+                            <button class="btn btn-side">Training</button>
+                            <button class="btn btn-side">Train for a career in tech</button>
+                            <button class="btn btn-side">Skills bootcamp</button>
+                            <button class="btn btn-side">SCS Frequently asked questions</button>
+                            <button class="btn btn-side">scion Collaborators</button>
+                            <button class="btn btn-side">our company</button>
+                            <button class="btn btn-side">why choose us?</button>
+                            <button class="btn btn-side">our culture</button>
+                            <button class="btn btn-side">our team</button>
+                            <button class="btn btn-side">our careers</button>
+                            <button class="btn btn-side">our benefits</button>
+                            <button class="btn btn-side">contact us</button>
+                            <button class="btn btn-side">london office</button>
+                            <button class="btn btn-side">Cambridge office</button>
+                            <button class="btn btn-side">wymondham office</button>
+                            <button class="btn btn-side">great Yarmouth office</button>
+                            <button class="btn btn-side">covid risk Assessments</button>
+                            <button class="btn btn-side">support</button>
+                        </div>
+                    </div>
+                    <div class="sidebar-large">
+                        <div class="sidebar-services">
+                            <p>Services</p>
+                            <ul>
+                                <li class="icon-dbl-arrows"> Bespoke Software</li>
+                                <li class="icon-dbl-arrows"> IT Support</li>
+                                <li class="icon-dbl-arrows"> Software Integrations</li>
+                                <li class="icon-dbl-arrows"> Digital Marketing</li>
+                                <li class="icon-dbl-arrows"> Web design</li>
+                                <li class="icon-dbl-arrows"> cyber security</li>
+                            </ul>
+                        </div>
+                        <div class="sidebar-work">
+                            <p>Our work</p>
+                            <ul>
+                                <li class="icon-dbl-arrows"> Case studies</li>
+                                <li class="icon-dbl-arrows"> our digital marketing clients</li>
+                                <li class="icon-dbl-arrows"> our website clients</li>
+                                <li class="icon-dbl-arrows"> our it clients</li>
+                                <li class="icon-dbl-arrows"> our bespoke software clients</li>
+                                <li class="icon-dbl-arrows"> our telecoms clients</li>
+                            </ul>
+                        </div>
+                        <div class="sidebar-knowledge">
+                            <p>Our knowledge</p>
+                            <ul>
+                                <li class="icon-dbl-arrows"> Technologies</li>
+                                <li class="icon-dbl-arrows"> industries</li>
+                                <li class="icon-dbl-arrows"> news</li>
+                                <li class="icon-dbl-arrows"> insights</li>
+                            </ul>
+                        </div>
+                        <div class="sidebar-training">
+                            <p>Training</p>
+                            <ul>
+                                <li class="icon-dbl-arrows"> train for a career in tech</li>
+                                <li class="icon-dbl-arrows"> skills bootcamp</li>
+                                <li class="icon-dbl-arrows"> scs frequently asked questions</li>
+                                <li class="icon-dbl-arrows"> scion Collaborators</li>
+                            </ul>
+                        </div>
+                        <div class="sidebar-company">
+                            <p>Our company</p>
+                            <ul>
+                                <li class="icon-dbl-arrows"> why choose us</li>
+                                <li class="icon-dbl-arrows"> our culture</li>
+                                <li class="icon-dbl-arrows"> our team</li>
+                                <li class="icon-dbl-arrows"> our careers</li>
+                                <li class="icon-dbl-arrows"> our benefits</li>
+                            </ul>
+                        </div>
+                        <div class="sidebar-contact">
+                            <p>contact us</p>
+                            <ul>
+                                <li class="icon-dbl-arrows"> Cambridge office</li>
+                                <li class="icon-dbl-arrows"> wymondham office</li>
+                                <li class="icon-dbl-arrows"> great Yarmouth office</li>
+                            </ul>
+                        </div>
+                        <p>covid risk assessments</p>
+                    </div>
+                </div>
+            </section>
+        </nav>
